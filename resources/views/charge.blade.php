689e8b199826d69679268299790c8217b032107b
mei0102 <akinaka52337@gmail.com>
2023-11-20 10:24:39 +0900

    edit charge

--- resources/views/charge.blade.php
+++ resources/views/charge.blade.php
@@ -1,7 +1,8 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('charge') }}
+            {{ __('charge') }}<br>
+            {{ __("(現在 charge 機能はメンテナンス中のため利用できません。)") }}
         </h2>
     </x-slot>
 
@@ -9,7 +10,9 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("不正チャージしてみよう") }}
+                <center><font size="5"><b>超peypey祭り</b></font></center>
+                <p>・特点1</p>
+                {{"　　　　"}}{{ __("1億円以上チャージすると期間限定でspctf{xxx} をプレゼント！") }}
                 </div>
             </div>
         </div>
